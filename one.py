2653c66cd251742d3abcb92972dc2c4088e6757a
NazmullSyntax <nazmullsiddik17@gmail.com>
2026-03-03 01:31:51 +0600

    just add some trick in py

--- one.py
+++ one.py
@@ -1,6 +1,24 @@
-num = int(input("Enter a number: "))
+name =("nazmul")
+print(name)
+# some practice
+x = str(5)
+y = "nazmul"
+print(x+y)
+#so
+x = 10j
+y = 3.14555555555555
+print(type(x+y))
+m = complex(10, 20)
+print(m)
+print(type(m))
+import random
+print(random.randint(1, 100))
+import math
+print(math.sqrt(16))
+#input
+name = input("Enter your name: ")
+print("Hello, " + name + "!")
+x = int(input("Enter a number: "))
+y = int(input("Enter another number: "))
+print("The sum is: " + str(x + y))
 
-if num % 2 == 0:
-    print("Even Number")
-else:
-    print("Odd Number")
