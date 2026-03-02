7bbeadae5249efcf2da947e312f008c1f578fe82
NazmullSyntax <nazmullsiddik17@gmail.com>
2026-03-03 03:13:38 +0600

    add vagses

--- two.py
+++ two.py
@@ -1,7 +1,14 @@
-dim = 10
-alu = 20
-kola = 30
-total = dim + alu + kola
+# dim = 10
+# alu = 20
+# kola = 30
+# total = dim + alu + kola
+# print("Total:", total)
+# final = total * 0.8
+# print("Final:", final)
+# vagses
+a = 10
+b = 3
+total = a //b
 print("Total:", total)
-final = total * 0.8
-print("Final:", final)
+
+
