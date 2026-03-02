76ce82893c29657ae542e7e9605d9099491380ee
NazmullSyntax <nazmullsiddik17@gmail.com>
2026-03-03 02:54:53 +0600

    add historic loop

--- one.py
+++ one.py
@@ -1,67 +1,96 @@
-# name =("nazmul")
-# print(name)
-# # some practice
-# x = str(5)
-# y = "nazmul"
-# print(x+y)
-# #so
-# x = 10j
-# y = 3.14555555555555
-# print(type(x+y))
-# m = complex(10, 20)
-# print(m)
-# print(type(m))
-# import random
-# print(random.randint(1, 100))
-# import math
-# print(math.sqrt(16))
-# #input
-# name = input("Enter your name: ")
-# print("Hello, " + name + "!")
-# x = int(input("Enter a number: "))
-# y = int(input("Enter another number: "))
-# print("The sum is: " + str(x + y))
+# # name =("nazmul")
+# # print(name)
+# # # some practice
+# # x = str(5)
+# # y = "nazmul"
+# # print(x+y)
+# # #so
+# # x = 10j
+# # y = 3.14555555555555
+# # print(type(x+y))
+# # m = complex(10, 20)
+# # print(m)
+# # print(type(m))
+# # import random
+# # print(random.randint(1, 100))
+# # import math
+# # print(math.sqrt(16))
+# # #input
+# # name = input("Enter your name: ")
+# # print("Hello, " + name + "!")
+# # x = int(input("Enter a number: "))
+# # y = int(input("Enter another number: "))
+# # print("The sum is: " + str(x + y))
 
-# # #if else
-#  a = 10
-#  b = 20
-#     if a > b:
-#         print("a is greater than b")
-#     else:
-#         print("b is greater than a")
+# # # #if else
+# #  a = 10
+# #  b = 20
+# #     if a > b:
+# #         print("a is greater than b")
+# #     else:
+# #         print("b is greater than a")
 
-#some
-# a = 5
-# b = 10
-# print("The sum is: " + str(a + b))
+# #some
+# # a = 5
+# # b = 10
+# # print("The sum is: " + str(a + b))
 
-# x = 10
-# y = 20
-# if x > y:
-#     print("x is greater than y")
-# else:    print("y is greater than x")
+# # x = 10
+# # y = 20
+# # if x > y:
+# #     print("x is greater than y")
+# # else:    print("y is greater than x")
 
-# a = 100
-# b = 200
-# if a>b:
-#     print("a is boro b")
-# else:    print("b is boro a")
+# # a = 100
+# # b = 200
+# # if a>b:
+# #     print("a is boro b")
+# # else:    print("b is boro a")
 
-# add some practice
-a = 5
-b = 10
-c = 100
-if a > b and a > c:
-    print("a is the greatest")
-elif b > a and b > c:
-    print("b is the greatest")
-else:    print("c is the greatest")
-# or function
-x = 10
-y = 20
-if x > y or x == y:
-    print("x is greater than or equal to y")
-else:    print("y is greater than x")
+# # add some practice
+# # a = 5
+# # b = 10
+# # c = 100
+# # if a > b and a > c:
+# #     print("a is the greatest")
+# # elif b > a and b > c:
+# #     print("b is the greatest")
+# # else:    print("c is the greatest")
+# # # or function
+# # x = 10
+# # y = 20
+# # if x > y or x == y:
+# #     print("x is greater than or equal to y")
+# # else:    print("y is greater than x")
+# # nested 
+# #  a = 100
+# #      if a > 50:
+# #     print("a is greater than 50")
+
+# # ki kora zai 
+#  x = 10
+
+# if x > 5:
+#     print("x is greater than 5")
+#     if x < 15:
+#     print("x is less than 15")
+a = 10
+b = 211
+if a>b:
+    print("run")
+elif a==b:
+    print("equal")
+else:    print("stop")
+
+
+
+    
+
+
+    
+    
+
+    
 
 
 
