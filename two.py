33b52ac7ef37bcaa0a69f79853371fa197ae9f26
NazmullSyntax <nazmullsiddik17@gmail.com>
2026-03-03 03:31:12 +0600

    add power

--- two.py
+++ two.py
@@ -10,9 +10,44 @@
 # b = 3
 # total = a //b
 # print("Total:", total)
+# a = 10
+# b = 3
+# total = a % b
+# print("Total:", total)
+# smalest and maximum
+#  a = [10, 20, 5, 15, 30]
+# # smallest = min(a)
+# smallest = a[0]
+# for i in a:
+#     if i < smallest:
+#         smallest = i
+# print("Smallest:", smallest)
+# # maximum = max(a)
+# maximum = a[0]
+# for i in a:
+#     if i > maximum:
+#         maximum = i
+# print("Maximum:", maximum)
+
+# sizes = [10, 20, 5, 15, 30]
+# smallest = min(sizes)
+# largest = max(sizes)
+# print("Smallest:", smallest)
+# print("Largest:", largest)
+#  a = 2
+#  b = 20
+#  result = a**b
+# print("Result:", result)
 a = 10
-b = 3
-total = a % b
-print("Total:", total)
+b = 20
+rs = 10**20
+print("power",rs)
+nazmul = 10
+huda = 20
+result = pow(nazmul, huda)
+print("Result:", result)
+
+
+
 
 
