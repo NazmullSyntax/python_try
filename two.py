fa28003176d290289549f1237decfccf52e14f11
NazmullSyntax <nazmullsiddik17@gmail.com>
2026-03-03 15:58:28 +0600

    add degress

--- two.py
+++ two.py
@@ -1,51 +1,95 @@
-# dim = 10
-# alu = 20
-# kola = 30
-# total = dim + alu + kola
-# print("Total:", total)
-# final = total * 0.8
-# print("Final:", final)
-# vagses
-# a = 10
-# b = 3
-# total = a //b
-# print("Total:", total)
-# a = 10
-# b = 3
-# total = a % b
-# print("Total:", total)
-# smalest and maximum
-#  a = [10, 20, 5, 15, 30]
-# # smallest = min(a)
-# smallest = a[0]
-# for i in a:
-#     if i < smallest:
-#         smallest = i
-# print("Smallest:", smallest)
-# # maximum = max(a)
-# maximum = a[0]
-# for i in a:
-#     if i > maximum:
-#         maximum = i
-# print("Maximum:", maximum)
+# # dim = 10
+# # alu = 20
+# # kola = 30
+# # total = dim + alu + kola
+# # print("Total:", total)
+# # final = total * 0.8
+# # print("Final:", final)
+# # vagses
+# # a = 10
+# # b = 3
+# # total = a //b
+# # print("Total:", total)
+# # a = 10
+# # b = 3
+# # total = a % b
+# # print("Total:", total)
+# # smalest and maximum
+# #  a = [10, 20, 5, 15, 30]
+# # # smallest = min(a)
+# # smallest = a[0]
+# # for i in a:
+# #     if i < smallest:
+# #         smallest = i
+# # print("Smallest:", smallest)
+# # # maximum = max(a)
+# # maximum = a[0]
+# # for i in a:
+# #     if i > maximum:
+# #         maximum = i
+# # print("Maximum:", maximum)
 
-# sizes = [10, 20, 5, 15, 30]
-# smallest = min(sizes)
-# largest = max(sizes)
-# print("Smallest:", smallest)
-# print("Largest:", largest)
-#  a = 2
+# # sizes = [10, 20, 5, 15, 30]
+# # smallest = min(sizes)
+# # largest = max(sizes)
+# # print("Smallest:", smallest)
+# # print("Largest:", largest)
+# #  a = 2
+# #  b = 20
+# #  result = a**b
+# # print("Result:", result)
+# # a = 10
+# # b = 20
+# # rs = 10**20
+# # print("power",rs)
+# # nazmul = 10
+# # huda = 20
+# # result = pow(nazmul, huda)
+# # print("Result:", result)
+# # import math
+# import math
+# a = 16
+# b =3.14
+# result = math.sqrt(a ** b)
+# print("Result:", round(result ,2))
+# # trangle
+# a = 5
+# b = 10
+# c = 15
+# s = (a + b + c) / 2
+# area = math.sqrt(s * (s - a) * (s - b) * (s - c))
+# print("Area of the triangle:", round(area, 2))
+#  import math
+#  a = 10
 #  b = 20
-#  result = a**b
-# print("Result:", result)
+#  area = 0.5 * a * b
+#  print("Area of the triangle:", round(area, 2))
+import math
 a = 10
 b = 20
-rs = 10**20
-print("power",rs)
-nazmul = 10
-huda = 20
-result = pow(nazmul, huda)
-print("Result:", result)
+c = 30
+s = (a + b + c) / 2
+area = math.sqrt(s * (s - a) * (s - b) * (s - c))
+print("Area of the triangle:", round(area, 2))
+value = 10
+rs = 20
+result = 0.5 * value * rs
+print("Area of the triangle:", round(result, 2))
+angle_radians = math.atan(rs/value)
+print("Angle in radians:", angle_radians)
+angle_degrees = math.degrees(angle_radians)
+print("Angle in degrees:", angle_degrees)
+
+
+
+
+
+
+
+
+
+
+
 
 
 
