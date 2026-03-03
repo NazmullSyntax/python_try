58ae2d25b617b073c2ca7808b0c5efa45efda983
NazmullSyntax <nazmullsiddik17@gmail.com>
2026-03-03 17:44:27 +0600

    loop f

--- loop.py
+++ loop.py
@@ -1,3 +1,12 @@
 donats = ["dim ","alu", "kola", "moris"]
 for donat in donats:
     print(donats)
+
+for i in range(5):
+    print(i)
+for i in range(2, 5):
+    print(i)
+for i in range(2, 10, 2):
+    print(i)
+    
+
