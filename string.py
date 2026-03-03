8ddb473af8641492d5752f399a0a75e87b68ba08
NazmullSyntax <nazmullsiddik17@gmail.com>
2026-03-03 16:21:24 +0600

    add string

---
+++ string.py
@@ -0,0 +1,6 @@
+tweet = "hellow my name is python and i am a programming language"
+print(tweet[0:5])
+print(tweet[6:8])
+print(tweet[11:15])
+print(tweet[0:5] + " " + tweet[6:8] + " " + tweet[11:15])
+print(tweet[0:5] + " " + tweet[6:8] + " " + tweet[11:15] + " " + tweet[16:18] + " " + tweet[19:21] + " " + tweet[22:28])
