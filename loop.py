078af0060d3142e8c767b879d641b3355d7dc6ba
NazmullSyntax <nazmullsiddik17@gmail.com>
2026-03-03 18:54:37 +0600

    add while

--- loop.py
+++ loop.py
@@ -1,12 +1,19 @@
-donats = ["dim ","alu", "kola", "moris"]
-for donat in donats:
-    print(donats)
+# donats = ["dim ","alu", "kola", "moris"]
+# for donat in donats:
+#     print(donats)
 
-for i in range(5):
-    print(i)
-for i in range(2, 5):
-    print(i)
-for i in range(2, 10, 2):
-    print(i)
+# for i in range(5):
+#     print(i)
+# for i in range(2, 5):
+#     print(i)
+# for i in range(2, 10, 2):
+#     print(i)..
+
+#while loop
+angle = 0
+while angle < 360:
+    print(angle)
+    angle += 90
     
+
 
